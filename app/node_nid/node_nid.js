f540717d2a186f6ad0dd0c57359acef10127f7d3
Martijn Braam <martijn@brixit.nl>
2014-09-24 12:51:35 +0200

    Added tags and user to node_nid

--- app/node_nid/node_nid.js
+++ app/node_nid/node_nid.js
@@ -9,6 +9,19 @@ angular.module('myApp.node_nid', ['ngRoute', 'drupalService'])
         });
     }])
 
-    .controller('NodeNidCtrl', function ($scope, $routeParams, Node) {
-        $scope.node = Node.get({nid: $routeParams.nid});
+    .controller('NodeNidCtrl', function ($scope, $routeParams, Node, User, TaxonomyTerm) {
+        $scope.tags = {}
+        $scope.node = Node.get({nid: $routeParams.nid}, function (node) {
+            if ($scope.node.uid[0].target_id == 0) {
+                $scope.node.user = anonymousUser;
+            } else {
+                $scope.node.user = User.get({uid: $scope.node.uid[0].target_id})
+            }
+
+            $scope.node.field_tags.forEach(function (element, index, array) {
+                if ($scope.tags[element.target_id] == undefined) {
+                    $scope.tags[element.target_id] = TaxonomyTerm.get({tid: element.target_id});
+                }
+            });
+        });
     });
