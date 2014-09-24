744ee2bfbebbab10aa489f40a79bae3ead708752
Martijn Braam <martijn@brixit.nl>
2014-09-24 12:48:35 +0200

    Added taxonomy terms

--- app/node/node.js
+++ app/node/node.js
@@ -9,7 +9,7 @@ angular.module('myApp.node', ['ngRoute', 'drupalService'])
         });
     }])
 
-    .controller('NodeCtrl', function ($scope, $http, Node, User) {
+    .controller('NodeCtrl', function ($scope, $http, Node, User, TaxonomyTerm) {
         var anonymousUser = {
             name: [
                 {
@@ -17,13 +17,22 @@ angular.module('myApp.node', ['ngRoute', 'drupalService'])
                 }
             ]
         }
+
+        $scope.tags = {};
+
         $scope.nodes = Node.query({}, function (nodes) {
             for (var i = 0; i < $scope.nodes.length; i++) {
-                if($scope.nodes[i].uid[0].target_id == 0){
+                if ($scope.nodes[i].uid[0].target_id == 0) {
                     $scope.nodes[i].user = anonymousUser;
-                }else {
+                } else {
                     $scope.nodes[i].user = User.get({uid: $scope.nodes[i].uid[0].target_id})
                 }
+
+                $scope.nodes[i].field_tags.forEach(function (element, index, array) {
+                    if ($scope.tags[element.target_id] == undefined) {
+                        $scope.tags[element.target_id] = TaxonomyTerm.get({tid: element.target_id});
+                    }
+                });
             }
         });
     });
