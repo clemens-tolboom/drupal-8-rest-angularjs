d7410e0625334a47859a5601c7781dc195be4829
Martijn Braam <martijn@brixit.nl>
2014-09-24 10:21:20 +0200

    Added taxonomy term support

--- app/node/node.js
+++ app/node/node.js
@@ -11,8 +11,24 @@ angular.module('myApp.node', ['ngRoute'])
 
     .controller('NodeCtrl', function ($scope, $http, $routeParams) {
         $scope.nid = $routeParams.nid;
-        $scope.node = {}
-        $http.get('/node/'+$scope.nid).success(function (data) {
+        $scope.node = {};
+        $scope.taxonomy = {};
+
+        $http.get('/node/' + $scope.nid).success(function (data) {
             $scope.node = data;
         });
+
+        $scope.$watch('node.field_tags', function (newValue, oldValue) {
+            if ($scope.node.field_tags != undefined) {
+
+
+                $scope.node.field_tags.forEach(function (element, index, array) {
+                    if ($scope.taxonomy[element.target_id] == undefined) {
+                        $http.get('/taxonomy_term/' + element.target_id).success(function (data) {
+                            $scope.taxonomy[element.target_id] = data;
+                        });
+                    }
+                });
+            }
+        }, true);
     });
