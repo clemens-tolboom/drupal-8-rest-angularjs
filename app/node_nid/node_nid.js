19af28e29105f8a10bcb03c554533ceed42aba83
Clemens Tolboom <clemens@build2be.com>
2015-02-02 19:06:02 +0100

    Oeps ... missed this.

--- app/node_nid/node_nid.js
+++ app/node_nid/node_nid.js
@@ -21,14 +21,6 @@ angular.module('myApp.node_nid', ['ngRoute', 'drupalService'])
         $scope.tags = {};
         // Fetch node entity for current nid
         $scope.node = Node.get({nid: $routeParams.nid}, function (node) {
-
-            // If the node isn't anonymous then fetch the user entity
-            if ($scope.node.uid[0].target_id == 0) {
-                $scope.node.user = anonymousUser;
-            } else {
-                $scope.node.user = User.get({uid: $scope.node.uid[0].target_id})
-            }
-
             $scope.breadcrumb = [
                 {
                     path: '#node',
