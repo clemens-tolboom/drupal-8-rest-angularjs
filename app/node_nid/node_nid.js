d8f7f5b01ae13914bf84b111a7566d37d9465098
Clemens Tolboom <clemens@build2be.com>
2015-02-09 21:39:16 +0100

    Make comment hal+json.

--- app/node_nid/node_nid.js
+++ app/node_nid/node_nid.js
@@ -26,24 +26,28 @@ angular.module('myApp.node_nid', ['ngRoute', 'drupalService'])
                     path: '#node',
                     title: 'Home'
                 }, {
-                    path: '#node/1',
+                    path: '#node/' + $routeParams.nid,
                     title: $scope.node.title[0].value
                 }
             ];
-
             // If the node isn't anonymous then fetch the user entity
             if ($scope.node._internals.uid[0].target_id == 0) {
                 $scope.node.user = anonymousUser;
             } else {
-                $scope.node.user = User.get({uid: $scope.node.uid[0].target_id})
+                $scope.node.user = User.get({uid: $scope.node._internals.uid[0].target_id})
             }
 
-            // Fetch the entity for every tag in this node.
-            $scope.node._internals.field_tags.forEach(function (element, index, array) {
-                if ($scope.tags[element.target_id] == undefined) {
-                    $scope.tags[element.target_id] = TaxonomyTerm.query({tid: element.target_id});
-                }
-            });
+            if ($scope.node._internals.field_tags) {
+                // Fetch the entity for every tag in this node.
+                $scope.node._internals.field_tags.forEach(function (element, index, array) {
+                    if ($scope.tags[element.target_id] == undefined) {
+                        $scope.tags[element.target_id] = TaxonomyTerm.query({tid: element.target_id});
+                    }
+                });
+            }
+        }, function(response) {
+            $scope.messages.push("Error");
+            console.log("ERROR?", response);
         });
 
 
@@ -52,11 +56,31 @@ angular.module('myApp.node_nid', ['ngRoute', 'drupalService'])
             nid: $routeParams.nid
         });
 
+        $scope.newComment = {
+            "subject": [
+                {
+                    "value": "Comment subject for Article 1"
+                }
+            ],
+            "_links": {
+                "type": {
+                    "href": "http://drupal.d8/rest/type/comment/comment"
+                },
+                "http://drupal.d8/rest/relation/comment/comment/entity_id": [
+                    {
+                        "href": "http://drupal.d8/node/" + $routeParams.nid
+                    }
+                ]
+            },
+            "comment_body" : [
+                {
+                    "value" : "<p>Comment body for Article 1</p>\r\n"
+                }
+            ]
+        };
+
         $scope.postComment = function () {
             // Post new comment to this node. $scope.newComment contains the http payload
-            $scope.newComment.entity_type = 'node';
-            $scope.newComment.field_name = 'comment';
-            $scope.newComment.entity_id = [{"target_id": $routeParams.nid}];
             Comment.post({}, $scope.newComment, function (response) {
                 // Comment posted, refresh the comment list
                 $scope.comments = Comment.query({nid: $routeParams.nid});
