6a7f79a8bc1e0affe96931a602710408321c6da1
Clemens Tolboom <clemens@build2be.com>
2015-02-10 08:54:05 +0100

    No need to fetch terms by id.

--- app/node_nid/node_nid.js
+++ app/node_nid/node_nid.js
@@ -18,7 +18,7 @@ angular.module('myApp.node_nid', ['ngRoute', 'drupalService'])
             ]
         };
 
-        $scope.tags = {};
+        $scope.tags = TaxonomyTerm.query();
         // Fetch node entity for current nid
         $scope.node = Node.fetch({nid: $routeParams.nid}, function (node) {
             $scope.breadcrumb = [
@@ -37,13 +37,6 @@ angular.module('myApp.node_nid', ['ngRoute', 'drupalService'])
             } else {
                 $scope.node.user = User.get({uid: $scope.node.uid[0].target_id})
             }
-
-            // Fetch the entity for every tag in this node.
-            $scope.node._internals.field_tags.forEach(function (element, index, array) {
-                if ($scope.tags[element.target_id] == undefined) {
-                    $scope.tags[element.target_id] = TaxonomyTerm.query({tid: element.target_id});
-                }
-            });
         });
 
 
